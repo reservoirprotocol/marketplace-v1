f4b9d6b73f88476e8eac9800f24a298506b33173
Diego Ramos <ramosdiego@pm.me>
2022-02-23 12:21:08 -0500

    improved toast ui

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react'
+import React, { FC, Fragment } from 'react'
+import { Transition } from '@headlessui/react'
 import toast, { Toast } from 'react-hot-toast'
 import {
-  HiCheckCircle,
-  HiExclamationCircle,
-  HiInformationCircle,
-  HiXCircle,
+  HiOutlineCheckCircle,
+  HiOutlineExclamationCircle,
+  HiOutlineInformationCircle,
+  HiOutlineXCircle,
+  HiX,
 } from 'react-icons/hi'
 
 type Props = {
@@ -19,28 +21,38 @@ type Props = {
 
 const Toast: FC<Props> = ({ t, toast, data: { kind, message, title } }) => {
   return (
-    <div
-      className={`${
-        t.visible ? 'animate-enter' : 'animate-leave'
-      } pointer-events-auto flex w-full max-w-md rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5`}
-    >
-      <div className="w-0 flex-1 p-4">
-        <div className="flex items-start">
-          <div className="flex-shrink-0 pt-0.5">{icons[kind]}</div>
-          <div className="ml-3 flex-1">
-            <p className="text-sm font-medium text-gray-900">{title}</p>
-            <p className="mt-1 text-sm text-gray-500">{message}</p>
+    <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+      <Transition
+        show={t.visible}
+        as={Fragment}
+        enter="transform ease-out duration-300 transition"
+        enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className="p-4">
+            <div className="flex items-start">
+              <div className="flex-shrink-0">{icons[kind]}</div>
+              <div className="ml-3 w-0 flex-1 pt-0.5">
+                <p className="text-sm font-medium text-gray-900">{title}</p>
+                <p className="mt-1 text-sm text-gray-500">{message}</p>
+              </div>
+              <div className="ml-4 flex flex-shrink-0">
+                <button
+                  className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  onClick={() => toast.dismiss(t.id)}
+                >
+                  <span className="sr-only">Close</span>
+                  <HiX className="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex border-l border-gray-200">
-        <button
-          onClick={() => toast.dismiss(t.id)}
-          className="flex w-full items-center justify-center rounded-none rounded-r-lg border border-transparent p-4 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        >
-          Close
-        </button>
-      </div>
+      </Transition>
     </div>
   )
 }
@@ -48,12 +60,14 @@ const Toast: FC<Props> = ({ t, toast, data: { kind, message, title } }) => {
 export default Toast
 
 const icons = {
-  error: <HiXCircle className="h-10 w-10 rounded-full text-red-700" />,
-  success: <HiCheckCircle className="h-10 w-10 rounded-full text-green-700" />,
+  error: <HiOutlineXCircle className="h-6 w-6 rounded-full text-red-400" />,
+  success: (
+    <HiOutlineCheckCircle className="h-6 w-6 rounded-full text-green-400" />
+  ),
   warning: (
-    <HiExclamationCircle className="h-10 w-10 rounded-full text-yellow-700" />
+    <HiOutlineExclamationCircle className="h-6 w-6 rounded-full text-yellow-400" />
   ),
   info: (
-    <HiInformationCircle className="h-10 w-10 rounded-full text-blue-700" />
+    <HiOutlineInformationCircle className="h-6 w-6 rounded-full text-blue-400" />
   ),
 }
