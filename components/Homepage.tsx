abf76101ea3cb359a898ccbb296035183fb1a7de
Diego Ramos <ramosdiego@pm.me>
2022-03-28 11:50:13 -0400

    added tagline support

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -6,6 +6,7 @@ import SearchCollection from './SearchCollections'
 
 const metaTitle = process.env.NEXT_PUBLIC_META_TITLE
 const metaDescription = process.env.NEXT_PUBLIC_META_DESCRIPTION
+const TAGLINE = process.env.NEXT_PUBLIC_TAGLINE
 
 type Props = {
   apiBase: string
@@ -29,6 +30,7 @@ const Homepage: FC<Props> = ({ apiBase }) => {
       content="Reservoir Market is an open source NFT marketplace powered by Reservoir"
     />
   )
+  const tagline = TAGLINE || 'Discover, buy and sell NFTs'
 
   return (
     <>
@@ -37,9 +39,7 @@ const Homepage: FC<Props> = ({ apiBase }) => {
         {description}
       </Head>
       <header className="col-span-full mb-10 mt-40">
-        <h1 className="reservoir-h1 text-center">
-          Discover, buy and sell NFTs
-        </h1>
+        <h1 className="reservoir-h1 text-center">{tagline}</h1>
       </header>
       <div className="col-span-full mb-12 md:col-span-4 md:col-start-3 lg:col-span-4 lg:col-start-5">
         <SearchCollection />
