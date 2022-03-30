b045a1a85956cec17f229e2eb25dae10393c70eb
Diego Ramos <ramosdiego@pm.me>
2022-03-30 11:11:51 -0400

    added primary color env support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,52 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 const FONT_FAMILY = process.env.NEXT_PUBLIC_FONT_FAMILY || 'Inter'
+const PRIMARY_COLOR = process.env.NEXT_PUBLIC_PRIMARY_COLOR || 'default'
+
+const primaryColors = {
+  red: {
+    100: '#fee2e2',
+    300: '#fca5a5',
+    500: '#ef4444',
+    700: '#b91c1c',
+    900: '#7f1d1d',
+  },
+  orange: {
+    100: '#ffedd5',
+    300: '#fdba74',
+    500: '#f97316',
+    700: '#c2410c',
+    900: '#7c2d12',
+  },
+  lime: {
+    100: '#ecfccb',
+    300: '#bef264',
+    500: '#84cc16',
+    700: '#4d7c0f',
+    900: '#365314',
+  },
+  green: {
+    100: '#dcfce7',
+    300: '#86efac',
+    500: '#22c55e',
+    700: '#15803d',
+    900: '#14532d',
+  },
+  blue: {
+    100: '#dbeafe',
+    300: '#93c5fd',
+    500: '#3b82f6',
+    700: '#1d4ed8',
+    900: '#1e3a8a',
+  },
+  default: {
+    100: '#F1E5FF',
+    300: '#E2CCFF',
+    500: '#A966FF',
+    700: '#7000FF',
+    900: '#430099',
+  },
+}
 
 module.exports = {
   content: [
@@ -25,13 +71,7 @@ module.exports = {
         'spin-reverse': 'spin 1s reverse linear infinite',
       },
       colors: {
-        primary: {
-          100: '#F1E5FF',
-          300: '#E2CCFF',
-          500: '#A966FF',
-          700: '#7000FF',
-          900: '#430099',
-        },
+        primary: primaryColors[PRIMARY_COLOR],
       },
     },
   },
