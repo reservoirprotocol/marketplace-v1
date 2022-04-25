ae7c8b919bd3eae1dc64268a509ea57be5e5bae0
Diego Ramos <ramosdiego@pm.me>
2022-04-25 14:06:40 -0400

    updated menu

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -1,8 +1,11 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import Link from 'next/link'
 import { FC, ReactNode } from 'react'
 import { FiMenu } from 'react-icons/fi'
-import { HiX } from 'react-icons/hi'
-import ConnectWallet from './ConnectWallet'
+import { HiOutlineLogout, HiX } from 'react-icons/hi'
+import { useAccount, useConnect } from 'wagmi'
+import { Balance } from './ConnectWallet'
+import EthAccount from './EthAccount'
 
 const NAVBAR_LOGO = process.env.NEXT_PUBLIC_NAVBAR_LOGO
 
@@ -15,6 +18,11 @@ type Props = {
 }
 
 const HamburgerMenu: FC<Props> = ({ search, externalLinks }) => {
+  const [{ data: connectData }, connect] = useConnect()
+  const [{ data: accountData, loading }, disconnect] = useAccount({
+    fetchEns: true,
+  })
+  const wallet = connectData.connectors[0]
   const logo = NAVBAR_LOGO || '/reservoir.svg'
 
   const hasExternalLinks = externalLinks.length > 0
@@ -24,7 +32,7 @@ const HamburgerMenu: FC<Props> = ({ search, externalLinks }) => {
         <FiMenu className="h-6 w-6" />
       </Dialog.Trigger>
 
-      <Dialog.Content className="fixed inset-0 transform rounded-md bg-white shadow-md">
+      <Dialog.Content className="fixed inset-0 z-10 transform rounded-md bg-white shadow-md">
         <div className="flex items-center justify-between gap-3 border-b border-neutral-300 p-4">
           <img src={logo} alt="" className="w-6 sm:block" />
           <div className="mx-auto h-full w-full max-w-md flex-grow">
@@ -48,22 +56,65 @@ const HamburgerMenu: FC<Props> = ({ search, externalLinks }) => {
             ))}
           </div>
         )}
-        <div className="p-4">
-          <ConnectWallet />
-        </div>
-        <a
+        {accountData ? (
+          <>
+            <div className="reservoir-label-l flex items-center justify-center border-b border-neutral-300 p-4 text-[#4B5563] hover:text-[#1F2937]">
+              <EthAccount
+                address={accountData.address}
+                ens={{
+                  avatar: accountData.ens?.avatar,
+                  name: accountData.ens?.name,
+                }}
+              />
+            </div>
+
+            <div className="reservoir-label-l flex items-center justify-between border-b border-neutral-300 p-4 text-[#4B5563] hover:text-[#1F2937]">
+              <span>Balance </span>
+              <span>
+                {accountData.address && (
+                  <Balance address={accountData.address} />
+                )}
+              </span>
+            </div>
+
+            <Link href={`/address/${accountData.address}`}>
+              <a className="group reservoir-label-l flex w-full cursor-pointer items-center justify-between rounded border-b border-neutral-300 p-4 text-[#4B5563] outline-none transition hover:bg-neutral-100 hover:text-[#1F2937] focus:bg-neutral-100">
+                Portfolio
+              </a>
+            </Link>
+
+            <button
+              key={wallet.id}
+              onClick={() => disconnect()}
+              className="group reservoir-label-l flex w-full cursor-pointer items-center justify-between gap-3 rounded border-b border-neutral-300 p-4 text-[#4B5563] outline-none transition hover:bg-neutral-100 hover:text-[#1F2937] focus:bg-neutral-100"
+            >
+              <span>Disconnect</span>
+              <HiOutlineLogout className="h-6 w-7" />
+            </button>
+          </>
+        ) : (
+          <button
+            key={wallet.id}
+            onClick={() => connect(wallet)}
+            className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9"
+          >
+            Connect Wallet
+          </button>
+        )}
+
+        {/* <a
           className="reservoir-h6 flex items-center gap-2 p-4"
           target="_blank"
           rel="noopener noreferrer"
           href={`https://metamask.io`}
         >
-          {/* <img
+          <img
             src="/icons/Twitter.svg"
             alt="Twitter Icon"
             className="h-6 w-6"
-          /> */}
+          />
           MetaMask
-        </a>
+        </a> */}
       </Dialog.Content>
     </Dialog.Root>
   )
