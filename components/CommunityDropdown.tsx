514d3bd424c28a6b0e578753a62dff7556d13a3f
Pedro <pedro.spinsomewebs@gmail.com>
2022-05-11 14:35:41 -0400

    Add font headings to community switcher, improve accessibility

--- components/CommunityDropdown.tsx
+++ components/CommunityDropdown.tsx
@@ -13,7 +13,7 @@ const CommunityDropdown: FC<Props> = ({ collections }) => {
 
   return (
     <DropdownMenu.Root open={open} onOpenChange={setOpen}>
-      <DropdownMenu.Trigger className="reservoir-h6 rounded dark:text-white dark:outline-none dark:ring-primary-900 dark:focus:ring-4">
+      <DropdownMenu.Trigger className="font-headings reservoir-h6 rounded dark:text-white dark:outline-none dark:ring-primary-900 dark:focus:ring-4">
         Collection
         <FiChevronDown
           className={`ml-3 inline text-[#525252] transition-transform dark:text-[#D4D4D4] ${
@@ -24,27 +24,27 @@ const CommunityDropdown: FC<Props> = ({ collections }) => {
 
       <DropdownMenu.Content
         sideOffset={43}
-        className="max-w-[300px] overflow-hidden rounded-2xl bg-white shadow-md radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:max-w-[422px]"
+        className="max-w-[300px] rounded-2xl bg-white shadow-md radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:max-w-[422px]"
       >
         {collections?.map((collection) => {
           return (
             <DropdownMenu.Item
               key={collection.collectionId}
-              className="first:rounded-t-2xl last:rounded-b-2xl"
+              className="reservoir-gray-dropdown-item overflow-hidden border-b p-0 outline-none first:rounded-t-2xl last:rounded-b-2xl last:border-b-0 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
             >
               <Link href={`/collections/${collection.collectionId}`}>
                 <a
                   onClick={() => {
                     setOpen(false)
                   }}
-                  className={`reservoir-gray-dropdown-item rounded-none border-b px-6 py-4 last:border-b-0 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800`}
+                  className={`flex items-center gap-2 rounded-none px-6 py-4 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800`}
                 >
                   <img
                     src={collection.image}
                     alt={`${collection.name} Collection Image`}
                     className="h-9 w-9 shrink-0 overflow-hidden rounded-full"
                   />
-                  <p className="reservoir-h6 truncate dark:text-white">
+                  <p className="font-headings reservoir-h6 truncate dark:text-white">
                     {collection.name}
                   </p>
                 </a>
