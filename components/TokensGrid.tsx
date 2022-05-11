cf72647d558cceda0e14a9b2aae8cd1ab55e64cc
Pedro <pedro.spinsomewebs@gmail.com>
2022-05-11 17:38:00 -0400

    Resolve conflicts

--- components/TokensGrid.tsx
+++ components/TokensGrid.tsx
@@ -97,62 +97,38 @@ const TokensGrid: FC<Props> = ({
                         width="250"
                         height="250"
                       />
-                    ) : (
-                      <div className="relative w-full">
-                        <div className="absolute inset-0 grid place-items-center backdrop-blur-lg">
-                          <div>
-                            <img
-                              src={optimizeImage(collectionImage, 250)}
-                              alt={`${token?.collection?.name}`}
-                              className="mx-auto mb-4 h-16 w-16 overflow-hidden rounded-full border-2 border-white"
-                              width="64"
-                              height="64"
-                            />
-                            <div className="reservoir-h6 font-headings text-white">
-                              No Content Available
-                            </div>
-                          </div>
-                        </div>
-                        <img
-                          src={optimizeImage(collectionImage, 250)}
-                          alt={`${token?.collection?.name}`}
-                          className="aspect-square w-full object-cover"
-                          width="250"
-                          height="250"
+                    </div>
+                  )}
+
+                  <p
+                    className="reservoir-subtitle mb-3 overflow-hidden truncate px-6 pt-4 dark:text-white lg:pt-3"
+                    title={token?.name || token?.tokenId}
+                  >
+                    {token?.name || `#${token?.tokenId}`}
+                  </p>
+                  <div className="flex items-center justify-between px-6 pb-4 lg:pb-3">
+                    <div>
+                      <div className="reservoir-subtitle text-gray-400">
+                        Offer
+                      </div>
+                      <div className="reservoir-h6 dark:text-white">
+                        <FormatEth
+                          amount={token?.topBidValue}
+                          maximumFractionDigits={4}
+                          logoWidth={7}
                         />
                       </div>
-                    )}
-
-                    <p
-                      className="reservoir-subtitle mb-3 overflow-hidden truncate px-6 pt-4 dark:text-white lg:pt-3"
-                      title={token?.name || token?.tokenId}
-                    >
-                      {token?.name || `#${token?.tokenId}`}
-                    </p>
-                    <div className="flex items-center justify-between px-6 pb-4 lg:pb-3">
-                      <div>
-                        <div className="reservoir-subtitle text-gray-400">
-                          Offer
-                        </div>
-                        <div className="reservoir-h6 dark:text-white">
-                          <FormatEth
-                            amount={token?.topBidValue}
-                            maximumFractionDigits={4}
-                            logoWidth={7}
-                          />
-                        </div>
+                    </div>
+                    <div className="text-right">
+                      <div className="reservoir-subtitle text-gray-400">
+                        Price
                       </div>
-                      <div className="text-right">
-                        <div className="reservoir-subtitle text-gray-400">
-                          Price
-                        </div>
-                        <div className="reservoir-h6 dark:text-white">
-                          <FormatEth
-                            amount={token?.floorAskPrice}
-                            maximumFractionDigits={4}
-                            logoWidth={7}
-                          />
-                        </div>
+                      <div className="reservoir-h6 dark:text-white">
+                        <FormatEth
+                          amount={token?.floorAskPrice}
+                          maximumFractionDigits={4}
+                          logoWidth={7}
+                        />
                       </div>
                     </div>
                   </div>
