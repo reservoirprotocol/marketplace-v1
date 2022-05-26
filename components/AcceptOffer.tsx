d6b84e628937405150962fb68950d87156dbc4f6
Diego Ramos <ramosdiego@pm.me>
2022-05-26 13:19:45 -0400

    disable accept button

--- components/AcceptOffer.tsx
+++ components/AcceptOffer.tsx
@@ -1,4 +1,3 @@
-import { Signer } from 'ethers'
 import { acceptOffer, Execute, paths } from '@reservoir0x/client-sdk'
 import React, {
   ComponentProps,
@@ -10,7 +9,7 @@ import React, {
 import { SWRResponse } from 'swr'
 import * as Dialog from '@radix-ui/react-dialog'
 import ModalCard from './modal/ModalCard'
-import { useAccount, useConnect, useSigner } from 'wagmi'
+import { useAccount, useSigner } from 'wagmi'
 import Toast from './Toast'
 import { SWRInfiniteResponse } from 'swr/infinite/dist/infinite'
 import { getDetails } from 'lib/fetch/fetch'
@@ -43,13 +42,11 @@ const AcceptOffer: FC<Props> = ({
   isInTheWrongNetwork,
   mutate,
   data,
-  children,
   signer,
   show,
   setToast,
 }) => {
   const [waitingTx, setWaitingTx] = useState<boolean>(false)
-  const { connect, connectors } = useConnect()
   const { data: accountData } = useAccount()
   const [steps, setSteps] = useState<Execute['steps']>()
   const [open, setOpen] = useState(false)
@@ -60,7 +57,7 @@ const AcceptOffer: FC<Props> = ({
   const [details, setDetails] = useState<SWRResponse<Details, any> | Details>()
 
   useEffect(() => {
-    if (data && open) {
+    if (data) {
       // Load data if missing
       if ('tokenId' in data) {
         getDetails(data.contract, data.tokenId, setDetails)
@@ -71,7 +68,7 @@ const AcceptOffer: FC<Props> = ({
         setCollection(data.collection)
       }
     }
-  }, [data, open])
+  }, [data])
 
   let tokenId: string | undefined = undefined
   let contract: string | undefined = undefined
@@ -89,8 +86,6 @@ const AcceptOffer: FC<Props> = ({
   // Set the token either from SWR or fetch
   let token: NonNullable<Details['tokens']>[0] = { token: undefined }
 
-  let topBuyValueExists = false
-
   // From fetch
   if (details && 'tokens' in details && details.tokens?.[0]) {
     token = details.tokens?.[0]
@@ -99,21 +94,6 @@ const AcceptOffer: FC<Props> = ({
   // From SWR
   if (details && 'data' in details && details?.data?.tokens?.[0]) {
     token = details.data?.tokens?.[0]
-    topBuyValueExists = !token?.market?.topBid?.value
-  }
-
-  const modalData = {
-    collection: {
-      name: collection?.collection?.name,
-    },
-    token: {
-      contract: token?.token?.contract,
-      id: token?.token?.tokenId,
-      image: token?.token?.image,
-      name: token?.token?.name,
-      topBuyValue: token?.market?.topBid?.value,
-      floorSellValue: token?.market?.floorAsk?.price,
-    },
   }
 
   const handleError: Parameters<typeof acceptOffer>[0]['handleError'] = (
@@ -186,7 +166,9 @@ const AcceptOffer: FC<Props> = ({
     <Dialog.Root open={open} onOpenChange={setOpen}>
       {show && (
         <Dialog.Trigger
-          disabled={waitingTx || topBuyValueExists || isInTheWrongNetwork}
+          disabled={
+            waitingTx || !token?.market?.topBid?.value || isInTheWrongNetwork
+          }
           onClick={() => {
             if (!taker || !tokenString) {
               dispatch({ type: 'CONNECT_WALLET', payload: true })
@@ -195,18 +177,12 @@ const AcceptOffer: FC<Props> = ({
 
             execute(tokenString, taker)
           }}
-          //className="btn-primary-outline w-full dark:text-white"
+          className="btn-primary-outline w-full dark:text-white"
         >
-          {children ? (
-            children
+          {waitingTx ? (
+            <CgSpinner className="h-4 w-4 animate-spin" />
           ) : (
-            <button className="btn-primary-outline w-full dark:text-white">
-              {waitingTx ? (
-                <CgSpinner className="h-4 w-4 animate-spin" />
-              ) : (
-                'Accept Offer'
-              )}
-            </button>
+            'Accept Offer'
           )}
         </Dialog.Trigger>
       )}
