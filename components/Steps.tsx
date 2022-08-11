039de91c3c56a62b01af05768e92c4e96fa8fc81
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-08-11 10:42:53 -0400

    Fix steps display

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -10,18 +10,18 @@ type Props = {
 const Steps: FC<Props> = ({ steps }) => {
   const validSteps =
     steps?.filter((step) => step.items && step.items.length > 0) || []
-  let firstInCompleteStepIndex = -1
-  let firstInCompleteStepItemIndex = -1
+  let firstIncompleteStepIndex = -1
+  let firstIncompleteStepItemIndex = -1
   const firstIncompleteStep = validSteps.find((step, i) => {
     if (!step.items) {
       return false
     }
 
-    firstInCompleteStepItemIndex = step.items.findIndex(
+    firstIncompleteStepItemIndex = step.items.findIndex(
       (item) => item.status == 'incomplete'
     )
-    if (firstInCompleteStepItemIndex >= 0) {
-      firstInCompleteStepIndex = i
+    if (firstIncompleteStepItemIndex >= 0) {
+      firstIncompleteStepIndex = i
       return true
     }
   })
@@ -33,9 +33,9 @@ const Steps: FC<Props> = ({ steps }) => {
           <div className="h-10 w-10">
             {error ? (
               <HiXCircle className="mx-auto -mt-1 h-10 w-10 flex-none text-red-600" />
-            ) : firstInCompleteStepIndex < index ? (
+            ) : firstIncompleteStepIndex > index ? (
               <HiCheckCircle className="mx-auto -mt-1 h-10 w-10 flex-none text-green-600" />
-            ) : firstInCompleteStepIndex === index ? (
+            ) : firstIncompleteStepIndex === index ? (
               <CgSpinner className="mr-1 ml-1 -mt-0.5 h-8 w-8 flex-none animate-spin text-black dark:text-white" />
             ) : (
               <div className="reservoir-h6 mr-1 ml-1 flex h-8 w-8 items-center justify-center rounded-full text-center font-headings ring-2 ring-inset ring-neutral-900 dark:text-white  dark:ring-white">
@@ -52,7 +52,7 @@ const Steps: FC<Props> = ({ steps }) => {
                 {error}
               </div>
             )}
-            {firstInCompleteStepIndex === index && (
+            {firstIncompleteStepIndex === index && (
               <>
                 <div className="reservoir-body mb-2.5 dark:text-white">
                   {description}
@@ -60,7 +60,7 @@ const Steps: FC<Props> = ({ steps }) => {
                 {firstIncompleteStep?.items &&
                   firstIncompleteStep.items.length > 1 && (
                     <div className="reservoir-body italic dark:text-white">
-                      {firstInCompleteStepItemIndex + 1} /{' '}
+                      {firstIncompleteStepItemIndex + 1} /{' '}
                       {firstIncompleteStep.items.length}
                     </div>
                   )}
