dc0c9f0d496e8562f8328997b0019a4fc9c8bf71
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-10-12 13:22:11 -0400

    Fix activity table row link

--- components/tables/ActivityTable.tsx
+++ components/tables/ActivityTable.tsx
@@ -120,6 +120,9 @@ const ActivityTableRow: FC<ActivityTableRowProps> = ({ sale }) => {
   const envChain = useEnvChain()
   const etherscanBaseUrl =
     envChain?.blockExplorers?.etherscan?.url || 'https://etherscan.io'
+  const href = sale?.token?.tokenId
+    ? `/${sale?.collection?.collectionId}/${sale?.token?.tokenId}`
+    : `/collections/${sale?.collection?.collectionId}`
 
   useEffect(() => {
     let toShortAddress = truncateAddress(sale?.toAddress || '')
@@ -233,10 +236,7 @@ const ActivityTableRow: FC<ActivityTableRowProps> = ({ sale }) => {
             </span>
           </div>
           <div className="flex items-center justify-between">
-            <Link
-              href={`/${sale?.collection?.collectionId}/${sale.token?.tokenId}`}
-              passHref
-            >
+            <Link href={href} passHref>
               <a className="flex items-center">
                 <Image
                   className="rounded object-cover"
@@ -334,10 +334,7 @@ const ActivityTableRow: FC<ActivityTableRowProps> = ({ sale }) => {
         </div>
       </td>
       <td className="px-6 py-4">
-        <Link
-          href={`/${sale.collection?.collectionId}/${sale.token?.tokenId}`}
-          passHref
-        >
+        <Link href={href} passHref>
           <a className="mr-2.5 flex items-center">
             <Image
               className="rounded object-cover"
