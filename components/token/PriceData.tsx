7fa972e7ab3e624947eabfe8d5b004c31deaf79d
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-10-31 17:17:28 -0400

    Fixes RES-875

--- components/token/PriceData.tsx
+++ components/token/PriceData.tsx
@@ -118,7 +118,8 @@ const PriceData: FC<Props> = ({ details, collection, isOwner }) => {
     accountData.isConnected &&
     token?.market?.topBid?.maker?.toLowerCase() ===
       accountData?.address?.toLowerCase()
-  const isListed = token?.market?.floorAsk?.price !== null
+  const isListed =
+    token?.market?.floorAsk?.price !== null && token?.token?.kind !== 'erc1155'
   const isInTheWrongNetwork = Boolean(signer && activeChain?.id !== +CHAIN_ID)
 
   const tokenId = token?.token?.tokenId
