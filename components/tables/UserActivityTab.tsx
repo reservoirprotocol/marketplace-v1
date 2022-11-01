ac967fa2e14d30d14ca25a8898e2cee44fbedeca
Pedro Cunha <pedro.spinsomewebs@gmail.com>
2022-11-01 13:58:23 -0400

    Filter for the activities (#568)
    * Filtering activity table by types
    
    * Fix mobile responsiveness for activity tab
    
    * Completes RES-881: filters hidden when no activity
    
    * Dark mode support for activity filters
    
    * Completes RES-882: fixes duplicates appearing when filters are added and removed
    
    * Resolves RES-893
    
    * Remove x icon from activity filters

--- components/tables/UserActivityTab.tsx
+++ components/tables/UserActivityTab.tsx
@@ -1,17 +1,25 @@
 import { useUsersActivity } from '@reservoir0x/reservoir-kit-ui'
 import ActivityTable from 'components/tables/ActivityTable'
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 type Props = {
   user?: string
 }
 
+type ActivityQuery = NonNullable<
+  Exclude<Parameters<typeof useUsersActivity>['1'], boolean>
+>
+
 const UserActivityTab: FC<Props> = ({ user }) => {
-  const query: Parameters<typeof useUsersActivity>['1'] = {
+  const [activityTypes, setActivityTypes] = useState<ActivityQuery['types']>([])
+  const query: ActivityQuery = {
     limit: 20,
+    types: activityTypes,
   }
   const data = useUsersActivity(user ? [user] : undefined, query, {
     revalidateOnMount: false,
+    fallbackData: [],
+    revalidateFirstPage: true,
   })
 
   useEffect(() => {
@@ -21,17 +29,20 @@ const UserActivityTab: FC<Props> = ({ user }) => {
     }
   }, [])
 
-  const noActivity = !data.isValidating && data.data.length === 0
-
-  if (noActivity) {
-    return (
-      <div className="reservoir-body mt-14 grid justify-center dark:text-white">
-        There hasn&apos;t been any activity yet.
-      </div>
-    )
-  }
-
-  return <ActivityTable data={data} />
+  return (
+    <ActivityTable
+      data={data}
+      types={activityTypes}
+      onTypesChange={(types) => {
+        setActivityTypes(types)
+      }}
+      emptyPlaceholder={
+        <div className="reservoir-body mt-14 grid justify-center dark:text-white">
+          There hasn&apos;t been any activity yet.
+        </div>
+      }
+    />
+  )
 }
 
 export default UserActivityTab
