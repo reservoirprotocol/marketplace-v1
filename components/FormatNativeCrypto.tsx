2fd064708461d30dce3ac4a152870a7ff3e896d7
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-11-07 15:26:40 -0500

    Remove hardcoded polygon bridge contract

--- components/FormatNativeCrypto.tsx
+++ components/FormatNativeCrypto.tsx
@@ -1,8 +1,6 @@
 import FormatCrypto from 'components/FormatCrypto'
 import { constants } from 'ethers'
-import useEnvChain from 'hooks/useEnvChain'
 import { FC, ComponentProps } from 'react'
-import { chain } from 'wagmi'
 
 type Props = ComponentProps<typeof FormatCrypto>
 
@@ -11,16 +9,8 @@ const FormatNativeCrypto: FC<Props> = ({
   maximumFractionDigits,
   logoWidth,
 }) => {
-  const envChain = useEnvChain()
-  let address = constants.AddressZero
-  let decimals = 18
-
-  switch (envChain?.id) {
-    case chain.polygon.id:
-      address = '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0'
-      decimals = chain.polygon.nativeCurrency?.decimals || decimals
-      break
-  }
+  const address = constants.AddressZero
+  const decimals = 18
 
   return (
     <FormatCrypto
