19978f00969673550f28c3c0209ef599e6d032fe
Ted Palmer <etpalmer17@gmail.com>
2022-11-14 17:10:57 -0500

    added cooldown timer to refresh button component

--- components/RefreshButton.tsx
+++ components/RefreshButton.tsx
@@ -1,6 +1,13 @@
 import { DateTime } from 'luxon'
 import router from 'next/router'
-import { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from 'react'
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { FiRefreshCcw } from 'react-icons/fi'
 
 type Props = {
@@ -11,17 +18,26 @@ type Props = {
 
 const RefreshButton: FC<Props> = ({ refreshData, isLoading, setIsLoading }) => {
   const [lastUpdated, setLastUpdated] = useState<DateTime>(DateTime.now())
-  const [timeDifference, setTimeDifference] = useState<string | undefined>('just now')
-  const countRef = useRef(lastUpdated);
+  const [timeDifference, setTimeDifference] = useState<string | undefined>(
+    'just now'
+  )
+  const countRef = useRef(lastUpdated)
+  const [cooldownEnabled, setCooldownEnabled] = useState(false)
 
   // Handle button click
   const handleRefreshData = async () => {
+    setCooldownEnabled(true)
     setIsLoading(true)
     refreshData()
     setLastUpdated(DateTime.now())
     setTimeDifference('just now')
-    await new Promise((resolve) => { setTimeout(resolve, 500)})
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500)
+    })
     setIsLoading(false)
+    setTimeout(() => {
+      setCooldownEnabled(false)
+    }, 30000)
   }
 
   // Handle route change
@@ -30,7 +46,7 @@ const RefreshButton: FC<Props> = ({ refreshData, isLoading, setIsLoading }) => {
       setIsLoading(true)
       setLastUpdated(DateTime.now())
       setTimeDifference('just now')
-      await new Promise(resolve => setTimeout(resolve, 500))
+      await new Promise((resolve) => setTimeout(resolve, 500))
       setIsLoading(false)
     }
     router.events.on('routeChangeStart', handleRouteChange)
@@ -41,39 +57,38 @@ const RefreshButton: FC<Props> = ({ refreshData, isLoading, setIsLoading }) => {
   }, [])
 
   useEffect(() => {
-    countRef.current = lastUpdated;
-  }, [lastUpdated]);
+    countRef.current = lastUpdated
+  }, [lastUpdated])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeDifference(countRef.current.plus({ seconds: 0 }).toRelative()?.replace(' seconds', 's')
-      .replace(' second', 's')
-      .replace(' minutes', 'm')
-      .replace(' minute', 'm')
-      .replace(' hour', 'hr')
-      .replace(' hours', 'hr'))
-    }, 30000);
+      setTimeDifference(
+        countRef.current
+          .plus({ seconds: 0 })
+          .toRelative()
+          ?.replace(' seconds', 's')
+          .replace(' second', 's')
+          .replace(' minutes', 'm')
+          .replace(' minute', 'm')
+          .replace(' hour', 'hr')
+          .replace(' hours', 'hr')
+      )
+    }, 30000)
 
-    return () => clearInterval(interval);
-  }, [lastUpdated]);
+    return () => clearInterval(interval)
+  }, [lastUpdated])
 
   return (
-    <div className='hidden lg:flex inline-flex items-center font-light min-w-70'>
+    <div className="min-w-70 inline-flex hidden items-center font-light lg:flex">
       <button
         onClick={() => handleRefreshData()}
-        className='btn-primary-outline px-2 rounded-full mr-2 border-none hover:border'
+        className="btn-primary-outline mr-2 rounded-full border-none px-2 hover:border disabled:text-neutral-400"
+        disabled={cooldownEnabled}
       >
-        <FiRefreshCcw
-          className='h-5 w-5'
-        />
+        <FiRefreshCcw className="h-5 w-5" />
       </button>
-      <span className='min-w-[140px]'>
-        {isLoading
-          ?
-          'Loading items...'
-          :
-          `Updated ${timeDifference}`
-        }
+      <span className="min-w-[140px]">
+        {isLoading ? 'Loading items...' : `Updated ${timeDifference}`}
       </span>
     </div>
   )
