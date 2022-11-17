4cd8c6d9f5c21145a21e8164bad7e887dc9b36a2
Ted Palmer <etpalmer17@gmail.com>
2022-11-17 11:11:44 -0500

    added rarity rank to tokens page

--- components/token/Owner.tsx
+++ components/token/Owner.tsx
@@ -4,14 +4,25 @@ import { FC } from 'react'
 import * as Tooltip from '@radix-ui/react-tooltip'
 import { FiAlertCircle } from 'react-icons/fi'
 import { useTokens } from '@reservoir0x/reservoir-kit-ui'
+import { Collection } from 'types/reservoir'
+import RarityTooltip from 'components/RarityTooltip'
 
 type Props = {
   details: ReturnType<typeof useTokens>['data'][0]
   bannedOnOpenSea: boolean
+  collection?: Collection
 }
 
-const Owner: FC<Props> = ({ details, bannedOnOpenSea }) => {
+const Owner: FC<Props> = ({ details, bannedOnOpenSea, collection }) => {
   const token = details?.token
+  const rarityRank = token?.rarityRank
+  const collectionSize = collection?.tokenCount
+    ? parseInt(collection?.tokenCount)
+    : undefined
+  const rankPercentile =
+    rarityRank && collectionSize
+      ? Math.floor((rarityRank / collectionSize) * 100)
+      : undefined
 
   const owner =
     token?.kind === 'erc1155' && details?.market?.floorAsk?.maker
@@ -21,7 +32,28 @@ const Owner: FC<Props> = ({ details, bannedOnOpenSea }) => {
   return (
     <div className="col-span-full md:col-span-4 lg:col-span-5 lg:col-start-2">
       <article className="col-span-full rounded-2xl border border-gray-300 bg-white p-6 dark:border-neutral-600 dark:bg-black">
-        <div className="reservoir-h3 mb-6 flex items-center gap-4 overflow-hidden font-headings dark:text-white">
+        {rarityRank && collectionSize && rankPercentile && (
+          <div className="mb-3 flex w-full">
+            <div className="hidden min-w-max items-center justify-between whitespace-nowrap rounded-md border border-neutral-300 px-2 py-1.5 text-sm sm:flex">
+              <img
+                src="/icons/rarity-icon.svg"
+                alt="Rarity icon"
+                className="mr-1 h-4 w-4"
+              />
+              <span className="mr-1 text-[#525252]">Rarity Score</span>
+              <span className="text-black">
+                {rarityRank} / {collectionSize} {`(${rankPercentile}%)`}
+              </span>
+            </div>
+            <div className="flex sm:hidden">
+              <RarityTooltip
+                rarityRank={rarityRank}
+                collectionSize={collectionSize}
+              />
+            </div>
+          </div>
+        )}
+        <div className="reservoir-h3 mb-3 flex items-center gap-4 overflow-hidden font-headings dark:text-white">
           <div>{token?.name || `#${token?.tokenId}`}</div>
           {bannedOnOpenSea && (
             <Tooltip.Provider>
