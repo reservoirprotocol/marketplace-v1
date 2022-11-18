28f937853c65902b81fd52ae29719c5fcd5049df
seb0zz <sebastian@elbstack.com>
2022-11-18 13:43:34 +0100

    fix: revert some changes

--- hooks/useCollectionsByContracts.ts
+++ hooks/useCollectionsByContracts.ts
@@ -5,35 +5,37 @@ import { useEffect } from 'react'
 import fetcher from 'lib/fetcher'
 import setParams from 'lib/params'
 import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite'
-import { PROXY_API_BASE } from "./useCollections";
+import { PROXY_API_BASE } from './useCollections'
 import { ContractMethodDoesNotExistError } from '@wagmi/core'
-
-const RESERVOIR_API_BASE = process.env.NEXT_PUBLIC_RESERVOIR_API_BASE
 
 type Collections = paths['/collections/v5']['get']['responses']['200']['schema']
 
-export default function useCollectionsByContracts(router: NextRouter, contracts: string[], fallback?: Collections) {
-    const { ref, inView } = useInView()
+export default function useCollectionsByContracts(
+  router: NextRouter,
+  contracts: string[],
+  fallback?: Collections
+) {
+  const { ref, inView } = useInView()
 
-    const pathname = PROXY_API_BASE ? `${PROXY_API_BASE}/collections/v5` : new URL('/collections/v5', RESERVOIR_API_BASE)
+  const pathname = `${PROXY_API_BASE}/collections/v5`
 
-    const sortBy = router.query['sort']?.toString()
+  const sortBy = router.query['sort']?.toString()
 
-    const collections = useSWRInfinite<Collections>(
-        (index, previousPageData) =>
-          getKey(pathname, sortBy, contracts, index, previousPageData),
-        fetcher,
+  const collections = useSWRInfinite<Collections>(
+    (index, previousPageData) =>
+      getKey(pathname, sortBy, contracts, index, previousPageData),
+    fetcher,
+    {
+      revalidateFirstPage: false,
+      fallbackData: [
         {
-          revalidateFirstPage: false,
-          fallbackData: [
-            {
-              collections: fallback?.collections,
-            },
-          ],
-        }
-      )
+          collections: fallback?.collections,
+        },
+      ],
+    }
+  )
 
-      // Fetch more data when component is visible
+  // Fetch more data when component is visible
   useEffect(() => {
     if (inView) {
       collections.setSize(collections.size + 1)
@@ -44,31 +46,31 @@ export default function useCollectionsByContracts(router: NextRouter, contracts:
 }
 
 export const getKey: (
-    pathname: URL | string,
-    sortBy: string | undefined,
-    contracts: string[],
-    ...base: Parameters<SWRInfiniteKeyLoader>
-  ) => ReturnType<SWRInfiniteKeyLoader> = (
-    pathname: URL | string,
-    sortBy: string | undefined,
-    contracts: string[],
-    index: number,
-    previousPageData: paths['/collections/v5']['get']['responses']['200']['schema']
-  ) => {
-    // Reached the end
-    if (previousPageData && !previousPageData?.continuation) return null
-  
-    let query: paths['/collections/v5']['get']['parameters']['query'] = {
-      limit: 20,
-      sortBy: '1DayVolume',
-      contract: contracts,
-    }
-  
-    if (previousPageData) query.continuation = previousPageData.continuation
-  
-    if (sortBy === '30DayVolume' || sortBy === '7DayVolume') query.sortBy = sortBy
-  
-    const href = setParams(String(pathname), query)
-  
-    return href
+  pathname: URL | string,
+  sortBy: string | undefined,
+  contracts: string[],
+  ...base: Parameters<SWRInfiniteKeyLoader>
+) => ReturnType<SWRInfiniteKeyLoader> = (
+  pathname: URL | string,
+  sortBy: string | undefined,
+  contracts: string[],
+  index: number,
+  previousPageData: paths['/collections/v5']['get']['responses']['200']['schema']
+) => {
+  // Reached the end
+  if (previousPageData && !previousPageData?.continuation) return null
+
+  let query: paths['/collections/v5']['get']['parameters']['query'] = {
+    limit: 20,
+    sortBy: '1DayVolume',
+    contract: contracts,
   }
+
+  if (previousPageData) query.continuation = previousPageData.continuation
+
+  if (sortBy === '30DayVolume' || sortBy === '7DayVolume') query.sortBy = sortBy
+
+  const href = setParams(String(pathname), query)
+
+  return href
+}
