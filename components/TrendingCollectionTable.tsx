7b8aaeb42ce2dccd11583d1088d6b3346dcd3139
Mike Lohmann <mike.h.lohmann@gmail.com>
2022-11-30 18:30:05 +0100

    Add the Advent Calendar Raffle Ticket to the whitelist
    New Christmas Raffle Tickets shall be shown prominently.

--- components/TrendingCollectionTable.tsx
+++ components/TrendingCollectionTable.tsx
@@ -19,6 +19,7 @@ const whitelistedCollections = [
   '0xfe7a68730499413a5819ab5f68191aac4b3ca7b5', // Autobahn Genesis Cars
   '0xc078e2264f06480380b1ddc31cbd573fb16ba01d', //, // Hodlers Club NFTs
   '0x67158b788968f78541847bce0bc9c495ed526831', // Autobahn Raffle Tickets
+  '0x2445c2468fbc777629e92c09f4a6f1973a6eb038', // Advent Calendar Raffle Tickets
 ]
 
 const TrendingCollectionTable: FC<Props> = () => {
