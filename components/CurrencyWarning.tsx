a786e59788c9c7a99b858ad99d5663c630dbbdca
Christian Eichinger <christianeichinger@MacBook-Pro-von-Christian.local>
2022-12-19 19:41:52 +0100

    update Warning to a maintenance info

--- components/CurrencyWarning.tsx
+++ components/CurrencyWarning.tsx
@@ -11,10 +11,12 @@ function CurrencyWarning() {
 
   const chainName = `${chain?.name} ${chain?.testnet ? 'Testnet' : ''}`
 
+  // All purchases are made in the native gas token TXL on the Autobahn Network, even if the frontend shows the Ethereum symbol. We are aware of it and will replace the ETH logo as soon as possible.
+
   if (chainId && signer && activeChain?.id === +chainId) {
     return (
       <div className="flex w-screen items-center justify-center gap-2 bg-[#FFA500] p-4 text-black">
-        <span>All purchases are made in the native gas token TXL on the Autobahn Network, even if the frontend shows the Ethereum symbol. We are aware of it and will replace the ETH logo as soon as possible.</span>
+        <span>MAINTENANCE: Currently we are working on a major update. During this update unexpected errors may occur.</span>
       </div>
     )
   }
